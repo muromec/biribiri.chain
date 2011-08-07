9d8fb1453d4d8546f63ee011aca8c0881b85069d
Ilya Petrov <ilya.muromec@gmail.com>
2011-08-07 05:00:51 +0300

    initial import of magic utility

---
+++ biribiri/chain/chain.py
@@ -0,0 +1,74 @@
+import types
+import logging
+
+
+class Stop(Exception):
+    pass
+
+class Derail(Exception):
+    pass
+
+def run(chain, chain_name="main", **ctx):
+    fchain = chain[:]
+
+    f = fchain.pop(0)
+
+    out = None
+
+    while f:
+        logging.debug('chain %s run %r from %r' % (chain_name, f, fchain))
+
+        try:
+            run = f
+            f = None
+            ret = run(upd_ctx=ctx, **ctx)
+
+            if isinstance(ret, list):
+                for add in ret:
+                    fchain.insert(0, add)
+
+            elif isinstance(ret, types.FunctionType):
+                f = ret
+
+            elif ret:
+                out = ret
+
+
+        except Stop, e:
+            break
+        
+        except Derail, e:
+            logging.debug("derail to %r" % (e.args,))
+            fchain = list(e.args)
+
+        if not f and fchain:
+            f = fchain.pop(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    def x(**ctx):
+        print 'x', ctx
+
+    def y(**ctx):
+        print 'y', ctx
+
+    def move(point, **ctx):
+        print 'move to', point
+        px,py = point
+
+        if not (px or py):
+            raise Derail(x,y)
+
+    def zog(upd_ctx, **ctx):
+        if 'point' in ctx:
+            return
+
+        upd_ctx.update({
+                "point": (0,0),
+        })
+
+        return [move]
+
+    print run([x, y, zog])
---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name = "biribiri.chain",
+    version = "0.1",
+    description = 'Chain runner utility',
+    author = 'Ilya Petrov',
+    author_email = 'ilya.muromec@gmail.com',
+    url = 'http://biribiri.enodev.org',
+    packages = ['biribiri', 'biribiri.chain',],
+    license = "BSDL",
+    scripts = [],
+)
